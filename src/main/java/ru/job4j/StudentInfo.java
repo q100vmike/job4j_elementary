e0e8317919b67c5bbd7984b45261be4bd116dc95
q100vmike <melkin@mail.ru>
2023-09-17 14:03:53 +0300

    3.3. Фиксация изменений на github [#243564]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Mike Kustov");
+        System.out.println("31.08.1985");
+    }
+}
