eaf98324d2bb529dda275353b1c4b34e473d7487
q100vmike <melkin@mail.ru>
2023-09-25 17:57:39 +0300

    9.3. Математические операторы и составные операторы присваивания [#504905]

---
+++ src/test/java/ru/job4j/converter/ConverterTest.java
@@ -0,0 +1,29 @@
+package ru.job4j.converter;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.withPrecision;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConverterTest {
+
+    @Test
+    void whenConvert140RblThen2Euro() {
+        float in = 140;
+        float expected = 2;
+        float out = Converter.rubleToEuro(in);
+        float eps = 0.0001f;
+        assertThat(out).isEqualTo(expected, withPrecision(eps));
+    }
+
+    @Test
+    void whenConvert180RblThen3Dollar() {
+        float in = 180;
+        float expected = 3;
+        float out = Converter.rubleToDollar(in);
+        float eps = 0.0001f;
+        assertThat(out).isEqualTo(expected, withPrecision(eps));
+    }
+
+}
