ad1b24d6a33c36c350d3d33d5b1e3d3ea4d1e1b0
q100vmike <melkin@mail.ru>
2023-09-27 22:15:34 +0300

    1.1. Что такое import [#504773 #447758]

--- src/main/java/ru/job4j/condition/TrgArea.java
+++ src/main/java/ru/job4j/condition/TrgArea.java
@@ -2,14 +2,12 @@ package ru.job4j.condition;
 
 public class TrgArea {
     public static double area(double a, double b, double c) {
-        double rsl = -1;
         double p = 0;
 
         p = ((a + b + c) / 2);
 
-        rsl = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+        return Math.sqrt(p * (p - a) * (p - b) * (p - c));
 
-        return rsl;
     }
 
     public static void main(String[] args) {
