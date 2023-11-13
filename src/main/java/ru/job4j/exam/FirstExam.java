e73a71ac3d260ed1f86c6bc57b4ef056bc41951a
q100vmike <melkin@mail.ru>
2023-11-13 14:03:12 +0300

    первый экзамен

--- src/main/java/ru/job4j/exam/FirstExam.java
+++ src/main/java/ru/job4j/exam/FirstExam.java
@@ -6,17 +6,17 @@ public class FirstExam {
         int j = arr.length - 1;
         int i = 0;
         int tmp;
-
+        boolean negflag = arr[arr.length - 1] <= 0;
         arr[0] *= arr[0];
 
         while (j != i) {
             arr[j] *= arr[j];
-            if (arr[i] > arr[j]) {
+            if (arr[i]  > arr[j]) {
                 tmp = arr[j];
                 arr[j] = arr[i];
                 arr[i] = tmp;
                 j--;
-                if (tmp == 0 && i != j) {
+                if (tmp == 0 && i != j || negflag && i != j) {
                     i++;
                     arr[i] *= arr[i];
                 }
