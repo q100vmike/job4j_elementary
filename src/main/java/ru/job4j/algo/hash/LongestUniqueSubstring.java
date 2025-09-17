5af3d5c60d6efd6e61d304dc869d842dc1b55544
q100vmike <melkin@mail.ru>
2025-09-17 15:26:20 +0300

    Алгоритмы с Hash структурами [#505154]

---
+++ src/main/java/ru/job4j/algo/hash/LongestUniqueSubstring.java
@@ -0,0 +1,37 @@
+package ru.job4j.algo.hash;
+
+import java.util.*;
+
+public class LongestUniqueSubstring {
+
+    public static String longestUniqueSubstring(String s) {
+        if (s == null || s.isEmpty()) {
+            return "";
+        }
+
+        HashSet<Character> set = new HashSet<>();
+        int start = 0;
+        String longestSubstring;
+        HashMap<Integer, String> strHashmap = new HashMap<>();
+
+        for (int end = 0; end < s.length(); end++) {
+            char currentChar = s.charAt(end);
+
+            while (set.contains(currentChar)) {
+                set.remove(s.charAt(start));
+                start++;
+            }
+            set.add(currentChar);
+
+            if (start < end + 1) {
+                longestSubstring = s.substring(start, end + 1);
+                strHashmap.put(longestSubstring.length(), longestSubstring);
+            }
+        }
+        Optional<String> strWithMaxLen = strHashmap.entrySet().stream()
+                .max(Map.Entry.comparingByKey())
+                .map(Map.Entry::getValue);
+
+        return strWithMaxLen.get();
+    }
+}
